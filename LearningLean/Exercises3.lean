9a6e5f38af99a0d94ef0f5763ef210874415753e
IainMcWhinnie <2566283m@student.gla.ac.uk>
2025-01-02 22:23:21 +0000

    Finished Exs 3

--- LearningLean/Exercises3.lean
+++ LearningLean/Exercises3.lean
@@ -225,3 +225,200 @@ example : ¬(p ↔ ¬p) := by
   have hp := h.mpr hnp
   apply hnp
   assumption
+
+
+-- Exercises from chapter 4 proved with tactics
+
+variable (α : Type) (p q : α → Prop)
+variable (r : Prop)
+
+
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := by
+  apply Iff.intro
+  . intro h
+    apply And.intro <;> intro x <;> have pq := h x
+    exact pq.left; exact pq.right
+  . intro h
+    intro x
+    have hp := h.left x; have hq := h.right x
+    apply And.intro <;> assumption
+
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := by
+  intro h₁ h₂
+  intro x
+  exact (h₁ x) (h₂ x)
+
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := by
+  intro h x
+  cases h with
+  | inl hl => apply Or.inl; apply hl;
+  | inr hr => apply Or.inr; apply hr;
+
+example : α → ((∀ x : α, r) ↔ r) := by
+  intro x
+  apply Iff.intro
+  . intro h
+    apply h; assumption
+  . intro hr x2
+    assumption
+
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := by
+  apply Iff.intro
+  . intro h
+    cases (em r) with
+    | inl hr => apply Or.inr; assumption
+    | inr hnr => apply Or.inl; intro x; cases h x with
+      | inl ht => assumption
+      | inr hf => exact absurd hf hnr
+  . intro h x
+    cases h with
+    | inl ht => apply Or.inl; exact ht x
+    | inr hf => apply Or.inr; assumption
+
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := by
+  apply Iff.intro
+  . intro h hr x
+    exact (h x) hr
+  . intro h x hr
+    exact (h hr) x
+
+section BarberShop
+
+variable (men : Type) (barber : men)
+variable (shaves : men → men → Prop)
+
+example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False := by
+  have problem := h barber
+  have doesNotShaveHimself := fun x => (problem.mp x) x
+  have doesShaveHimself := problem.mpr doesNotShaveHimself
+  exact absurd doesShaveHimself doesNotShaveHimself
+
+end BarberShop
+
+example : (∃ x : α, r) → r := by
+  intro h
+  match h with
+  | ⟨x, hx⟩ => assumption
+
+example (a : α) : r → (∃ x : α, r) := by
+  intro hr
+  apply Exists.intro
+  assumption; assumption
+
+example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := by
+  apply Iff.intro
+  . intro h
+    match h with
+    | ⟨x, hx⟩ => apply And.intro; apply Exists.intro x; exact hx.left; exact hx.right
+  . intro h
+    match h.left with
+    | ⟨x, hx⟩ => apply Exists.intro x; apply And.intro; repeat assumption; exact h.right
+
+
+example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := by
+  apply Iff.intro
+  . intro h
+    match h with
+    | ⟨x, hx⟩ => cases hx; apply Or.inl; apply Exists.intro x; assumption;
+                  apply Or.inr; apply Exists.intro x; assumption
+  . intro h
+    cases h with
+    | inl hl => match hl with
+      | ⟨x, hx⟩ => apply Exists.intro x; apply Or.inl; assumption
+    | inr hr => match hr with
+      | ⟨x, hx⟩ => apply Exists.intro x; apply Or.inr; assumption
+
+example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := by
+  apply Iff.intro
+  . intro h
+    intro nh
+    match nh with
+    | ⟨x, hx⟩ => exact hx (h x)
+  . intro h
+    intro x
+    cases (em (p x)) with
+    | inl hpx => assumption
+    | inr nhpx => apply False.elim; exact h ⟨x, nhpx⟩
+
+example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := by
+  apply Iff.intro
+  intro h hn
+  match h with
+  | ⟨x, hx⟩ => exact (hn x) hx
+  intro h
+  apply byContradiction
+  intro nh
+  apply h
+  intro x
+  intro h2
+  exact nh (by apply Exists.intro x; assumption)
+
+example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := by
+  apply Iff.intro
+  . intro h
+    intro x
+    intro hpx
+    apply h
+    apply Exists.intro x
+    assumption
+  . intro h
+    intro he
+    match he with
+    | ⟨x, hx⟩ => exact (h x) hx
+
+example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := by
+  apply Iff.intro
+  . intro h
+    apply byContradiction
+    intro nh
+    apply h
+    intro x
+    cases (em (p x)) with
+    | inl hpx => assumption
+    | inr hnpx => exact False.elim (nh (by apply Exists.intro x; exact hnpx))
+
+example : (∀ x, p x → r) ↔ (∃ x, p x) → r := by
+  apply Iff.intro
+  . intro h
+    intro h2
+    match h2 with
+    | ⟨x, hx⟩ => exact (h x) hx
+  . intro h
+    intro x
+    intro hpx
+    exact h (by apply Exists.intro x; assumption)
+
+example (a : α) : (∃ x, p x → r) ↔ (∀ x, p x) → r := by
+  apply Iff.intro
+  . intro h
+    intro h2
+    match h with
+    | ⟨x, hx⟩ => apply hx; exact h2 x
+  . intro h
+    cases (em (∀ x, p x)) with
+    | inl hallpx => admit
+    | inr hnallpx =>
+    have w : (∃ (x:α), ¬ p x):= (by
+    apply byContradiction;
+    intro nh; apply hnallpx; intro x; cases (em (p x)); assumption;
+    apply False.elim; exact nh (by apply Exists.intro x; assumption))
+    match w with
+    | ⟨x, hx⟩ => apply Exists.intro x; intro hpx; exact absurd hpx hx
+
+example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) := by
+  apply Iff.intro
+  . intro h hr
+    match h with
+    | ⟨x, hx⟩ => apply Exists.intro x; exact hx hr
+  . intro h
+    cases (em r) with
+    | inl hr => match h hr with
+      | ⟨x, hx⟩ => apply Exists.intro x; intro _; assumption
+    | inr hnr => apply Exists.intro a; intro hr; exact absurd hr hnr
+
+
+-- Ex 2 : One line proof question
+
+example (p q r : Prop) (hp : p)
+        : (p ∨ q ∨ r) ∧ (q ∨ p ∨ r) ∧ (q ∨ r ∨ p) := by
+  constructor <;> (try constructor) <;> repeat (first | apply Or.inl; assumption | apply Or.inr | assumption)
